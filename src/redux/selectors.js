577e2d6a71420b7f0b99287d44c4d36be34bc9fb
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-04-25 10:24:04 +0300

    add selectors redux

---
+++ src/redux/selectors.js
@@ -0,0 +1,7 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectUsers = (state) => state.users.items;
+export const selectIsLoading = (state) => state.users.isLoading;
+export const selectError = (state) => state.users.error;
+export const selectLoadMore = (state) => state.users.canLoadMore;
+export const selectUserFollow = (state) => state.following.followingUsers;
