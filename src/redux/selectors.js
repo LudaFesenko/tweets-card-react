59b80c571e5b96a761ed18e5f140afa62f646d30
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-04-25 10:38:11 +0300

    fix

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,5 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-export const selectUsers = (state) => state.users.items;
-export const selectIsLoading = (state) => state.users.isLoading;
-export const selectError = (state) => state.users.error;
-export const selectLoadMore = (state) => state.users.canLoadMore;
-export const selectUserFollow = (state) => state.following.followingUsers;
+export const selectUsers = state => state.users.items;
+export const selectIsLoading = state => state.users.isLoading;
+export const selectError = state => state.users.error;
+export const selectLoadMore = state => state.users.canLoadMore;
+export const selectUserFollow = state => state.following.followingUsers;
