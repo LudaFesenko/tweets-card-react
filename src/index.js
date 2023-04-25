974bf6dbea97dbefeb6bb8b759da764eb557cb19
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-04-25 16:53:25 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/tweets-card-react">
           <App />
         </BrowserRouter>
       </PersistGate>
