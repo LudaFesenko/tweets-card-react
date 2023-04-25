ac065ef6475d2e78ac26998e1cff600a9ecc91b6
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-04-25 16:53:05 +0300

    some fix redux

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = "https://6443cf3c90738aa7c0781a5e.mockapi.io";
+axios.defaults.baseURL = 'https://6443cf3c90738aa7c0781a5e.mockapi.io';
 
 export const fetchUsers = createAsyncThunk(
-  "users/fetchAll",
+  'users/fetchAll',
   async ({ page, abortController }, thunkAPI) => {
     try {
-      const { data } = await axios.get("/users", {
+      const { data } = await axios.get('/users', {
         signal: abortController.signal,
         params: { page, limit: 3 },
       });
